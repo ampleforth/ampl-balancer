20cf57cd33a8d1c6d9d21fdcba9095af756f4e8a
Fernando Martinelli <fernando@balancer.finance>
2020-06-10 13:30:30 -0300

    Create readme.mm

---
+++ crp/readme.mm
@@ -0,0 +1,7 @@
+##CRP - Configurable Rights Pool
+This is a smart pool factory that allows anyone to deploy smart pools with 4 different rights that can be individually chosen: 
+
+1) pausableSwap: pool creator can pause swaps
+2) configurableSwapFee: pool creator can change trading fees
+3) configurableWeights: pool creator can change weights
+4) configurableAddRemoveTokens: pool creator can add/remove tokens
