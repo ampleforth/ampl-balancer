5fcbea0925dc4f060edb00652a21adc06cc98a80
nithin <nithin@fragments.org>
2020-09-11 19:28:38 -0400

    fixed arg type

--- contracts/AmplElasticCRPWrapper.sol
+++ contracts/AmplElasticCRPWrapper.sol
@@ -16,7 +16,7 @@ interface IAmplElasticCRP {
 contract AmplElasticCRPWrapper {
     event ErrorReason(bytes reason);
 
-    function safeResync(address crp, IBPool bpool, address token) public {
+    function safeResync(address crp, address bpool, address token) public {
 
         try IAmplElasticCRP(crp).resyncWeight(token) {
             // no-op : Resync call success
